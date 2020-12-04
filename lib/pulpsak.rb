bd6ece2e7bdb1d941070e4009cb8ac7370a361d1
Aran Cox <aran-cox@uiowa.edu>
2020-12-04 11:42:07 -0600

    include pulpcore bindings in order to wait on async tasks

--- lib/pulpsak.rb
+++ lib/pulpsak.rb
@@ -1,8 +1,16 @@
 require "pulpsak/version"
 require 'pulp_rpm_client'
+require 'pulpcore_client'
 
 module Pulpsak
   class Error < StandardError; end
+  PulpcoreClient.configure do |config|
+    # Configure HTTP basic authorization: basicAuth
+    config.username = 'admin'
+    config.password = 'testing'
+    config.ssl_verify = false
+    config.host = 'localhost:8080'
+  end
   PulpRpmClient.configure do |config|
     # Configure HTTP basic authorization: basicAuth
     config.username = 'admin'
@@ -40,5 +48,18 @@ module Pulpsak
       end
     end
     return nil
+  end
+  def self.distribution_inspect_by_name(name)
+    api_instance = PulpRpmClient::DistributionsRpmApi.new
+    api_instance.list.results.each do |dist|
+      if dist.name == name
+        return dist
+      end
+    end
+    return nil
+  end
+  def self.task_inspect(href)
+    api_instance = PulpcoreClient::TasksApi.new
+    return api_instance.read(href)
   end
 end
